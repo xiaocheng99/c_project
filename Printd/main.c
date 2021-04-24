401d2027323b2f93e4fad53fd1d2f64ee5ee9902
xiaocheng99 <1002620235@qq.com>
2021-04-24 13:50:20 +0000

    the function of printd

---
+++ Printd/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void printd (int n)
+{
+	if(n<0)
+	{
+		putchar('-');
+		n = -n;
+	}
+	if (n/10)
+	{
+		printd(n/10);
+	}
+	putchar( n % 10 + '0');
+}
+int main()
+{
+	int num;
+	scanf("%d",&num);
+	printd(num);
+	printf("\n");
+	return 0;
+}
