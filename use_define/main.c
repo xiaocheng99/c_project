b562ed4e504f67135066af877758d0330e8db8ae
xiaocheng99 <1002620235@qq.com>
2021-04-27 04:20:49 +0000

    update the define funtion

--- use_define/main.c
+++ use_define/main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
-#define swap(A,B) ((A>B)?( A):( B))
+#define dprint(expr)	printf(#expr " = %d\n",expr); 
+#define max(A,B) 	((A>B)?( A):( B))
 
 int main()
 {
-	int a,b;
+	int a,b,temp;
 	printf("please enter the number of a and b:\n");
 	scanf("%d %d",&a,&b);
-	a=swap(a,b);
-	printf("the number is:%d\n",a);
+	dprint(a*b);
+	//a=max(a,b);
+	temp = max(a++,b++);
+	printf("the max number is:%d\n",temp);
 	return 0;
 }
