9df23571e35fd9931b43b60e8a1ff82c4ceaaa81
xiaocheng99 <1002620235@qq.com>
2021-04-27 13:26:06 +0000

    update the definr paste

--- use_define/main.c
+++ use_define/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-#define dprint(expr)	printf(#expr " = %d\n",expr); 
-#define max(A,B) 	((A>B)?( A):( B))
+#define dprint(expr)		printf(#expr " = %d\n",expr); 
+#define max(A,B) 		((A>B)?( A):( B))
+#define paste(front , back)	front ## back
 
 int main()
 {
 	int a,b,temp;
+	printf("%d",paste(1,2));
 	printf("please enter the number of a and b:\n");
 	scanf("%d %d",&a,&b);
 	dprint(a*b);
