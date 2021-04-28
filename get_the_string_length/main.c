505aeb4bf14b9966649181bcac62a82149007a55
xiaocheng99 <1002620235@qq.com>
2021-04-28 10:35:20 +0000

    the function of strlen with pointer

--- get_the_string_length/main.c
+++ get_the_string_length/main.c
@@ -8,13 +8,14 @@ int strlen (char *s)
 		n++;
 	}
 
-	return 0;
+	return n;
 }
 
 int main()
 {
-	
+	char str[] = "i hell0";
+	char *p = str;		/* the arry also is a pointer that point the first postion*/
+	printf("%c\n",*(p+1));
 	printf("this function will count the char number\n");
-	printf("please enter the charcters:\n");
-	printf("the string have %d characters\n",strlen("hello world"));
+	printf("the string have %d characters\n",strlen(p));
 }
