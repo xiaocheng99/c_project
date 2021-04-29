8cffbee128fce4eb167b106247c13af1b1ea4b49
xiaocheng99 <1002620235@qq.com>
2021-04-29 01:50:32 +0000

    add another function that use point

--- get_the_string_length/main.c
+++ get_the_string_length/main.c
@@ -11,6 +11,16 @@ int strlen (char *s)
 	return n;
 }
 
+int p_strlen(char *s)
+{
+	char *p = s;
+	while( *p !='\0' )
+	{
+		p++;
+	}
+	return p - s;
+}
+
 int main()
 {
 	char str[] = "i hell0";
@@ -20,5 +30,7 @@ int main()
 	printf("the string have %d characters\n",strlen(p));
 	printf("the string have %d characters\n",strlen(str));
 	printf("the string have %d characters\n",strlen(&str[0]));
+	printf("the string have %d characters\n",p_strlen(p));
+
 
 }
